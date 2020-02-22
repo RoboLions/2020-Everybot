a8551f9e54c0a806dbaa77e21da252ee748ec1c5
Ava B <avabyrd96@gmail.com>
2020-02-22 12:07:45 -0500

    Added Near Trench Path

---
+++ src/main/java/frc/robot/commands/AutonomousPaths/NearTrenchPath.java
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.AutonomousPaths;
+
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+import frc.robot.commands.AutoMove;
+import frc.robot.commands.IntakeBalls;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class NearTrenchPath extends SequentialCommandGroup {
+  /**
+   * Robot moves from the initiation line towards the goal and then emptys into the goal
+   */
+
+  public NearTrenchPath(final DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem) {
+    // Add your commands in the super() call, e.g.
+    // super(new FooCommand(), new BarCommand());
+    super(new AutoMove(driveSubsystem, 2.99 /*3.0969585*/), //3.0969585 
+        /* Arm Down */
+        new IntakeBalls(intakeSubsystem));
+
+  }
+}
